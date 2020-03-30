82286e1d5b56733ae05944a0a763249e20456e08
David Millman <dave@cs.unc.edu>
2020-03-30 16:07:46 -0600

    Rename input vars

--- include/csci441/tuple.h
+++ include/csci441/tuple.h
@@ -66,9 +66,11 @@ public:
     }
 };
 
-float dot(const Tuple& t1, const Tuple& t2) {
-    return t1.x*t2.x + t1.y*t2.y + t1.z*t2.z + t1.w*t2.w;
+
+float dot(const Tuple& a, const Tuple& b) {
+    return a.x*b.x + a.y*b.y + a.z*b.z + a.w*b.w;
 }
+
 
 float magnitude(const Tuple& t) {
     return std::sqrt(dot(t, t));
